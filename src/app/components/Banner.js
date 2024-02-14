737fb8eab18fdb6924c44eddc5f49dd8150e2f20
Bryant Richards <dragonseyeimages@gmail.com>
2024-02-14 12:16:14 -0600

    updates banner section

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+import { FaXTwitter } from 'react-icons/fa6'
 
 const Banner = () => {
   return (
@@ -8,8 +10,8 @@ const Banner = () => {
         <div className='d-table-cell'>
           <div className='container-fluid'>
             <div className='main-banner-content'>
-              <span className='sub-title'>Lionel</span>
-              <h1>Mason</h1>
+              <span className='sub-title'>Dickson</span>
+              <h1>Dolphins</h1>
             </div>
           </div>
         </div>
@@ -19,34 +21,24 @@ const Banner = () => {
         <div className='container-fluid'>
           <div className='row align-items-center'>
             <div className='col-lg-6 col-sm-6'>
-              <span className='email-link'>hello@lionelmason.com</span>
-            </div>
-
-            <div className='col-lg-6 col-sm-6'>
               <ul className='social'>
                 <li>
+                  <a
+                    href='https://www.facebook.com/DicksonDolphins'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <FaFacebook />
+                  </a>
+                </li>
+                <li>
                   <Link href='#'>
-                    <i className='flaticon-facebook'></i>
+                    <FaXTwitter />
                   </Link>
                 </li>
                 <li>
                   <Link href='#'>
-                    <i className='flaticon-twitter-1'></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link href='#'>
-                    <i className='flaticon-instagram-1'></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link href='#'>
-                    <i className='flaticon-linkedin-1'></i>
-                  </Link>
-                </li>
-                <li>
-                  <Link href='#'>
-                    <i className='flaticon-youtube-1'></i>
+                    <FaInstagram />
                   </Link>
                 </li>
               </ul>
