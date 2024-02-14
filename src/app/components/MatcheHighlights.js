a1d4c823bffef52b545f9c3592c624416d81b729
Bryant Richards <dragonseyeimages@gmail.com>
2024-02-14 11:31:23 -0600

    templates project

---
+++ src/app/components/MatcheHighlights.js
@@ -0,0 +1,180 @@
+import React, { Component } from 'react'
+import Link from 'next/link'
+import dynamic from 'next/dynamic'
+import OwlCarousel from 'react-owl-carousel3'
+// const OwlCarousel = dynamic(import('react-owl-carousel3'));
+
+import ModalVideo from 'react-modal-video'
+import 'react-modal-video/css/modal-video.min.css'
+
+const options = {
+  loop: true,
+  nav: true,
+  dots: false,
+  autoplayHoverPause: true,
+  autoplay: false,
+  items: 1,
+  navText: ["<i class='flaticon-left'></i>", "<i class='flaticon-right'></i>"],
+}
+
+class MatcheHighlights extends Component {
+  // Carousel
+  state = {
+    display: false,
+  }
+
+  componentDidMount() {
+    this.setState({ display: true })
+  }
+
+  // Popup Video
+  state = {
+    isOpen: false,
+  }
+  openModal = () => {
+    this.setState({ isOpen: true })
+  }
+
+  render() {
+    return (
+      <>
+        <div id='highlights'>
+          {this.state.display ? (
+            <OwlCarousel
+              className='matches-highlights-slides owl-carousel owl-theme'
+              {...options}
+            >
+              <div className='single-matches-highlights-item highlights-bg1'>
+                <div className='container'>
+                  <div className='row align-items-center'>
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='content'>
+                        <h3>Matches Highlights</h3>
+                        <span>Champions League - 20 April, 2020</span>
+                      </div>
+                    </div>
+
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='highlights-video'>
+                        <Link
+                          href='#play-video'
+                          onClick={(e) => {
+                            e.preventDefault()
+                            this.openModal()
+                          }}
+                          className='video-btn popup-youtube'
+                        >
+                          <span>Play Video</span>
+                          <i className='flaticon-play-button'></i>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='single-matches-highlights-item highlights-bg2'>
+                <div className='container'>
+                  <div className='row align-items-center'>
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='content'>
+                        <h3>Matches Highlights</h3>
+                        <span>Premier League - 19 April, 2020</span>
+                      </div>
+                    </div>
+
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='highlights-video'>
+                        <Link
+                          href='#play-video'
+                          onClick={(e) => {
+                            e.preventDefault()
+                            this.openModal()
+                          }}
+                          className='video-btn popup-youtube'
+                        >
+                          <span>Play Video</span>
+                          <i className='flaticon-play-button'></i>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='single-matches-highlights-item highlights-bg3'>
+                <div className='container'>
+                  <div className='row align-items-center'>
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='content'>
+                        <h3>Matches Highlights</h3>
+                        <span>Champions League - 18 April, 2020</span>
+                      </div>
+                    </div>
+
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='highlights-video'>
+                        <Link
+                          href='#play-video'
+                          onClick={(e) => {
+                            e.preventDefault()
+                            this.openModal()
+                          }}
+                          className='video-btn popup-youtube'
+                        >
+                          <span>Play Video</span>
+                          <i className='flaticon-play-button'></i>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className='single-matches-highlights-item highlights-bg4'>
+                <div className='container'>
+                  <div className='row align-items-center'>
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='content'>
+                        <h3>Matches Highlights</h3>
+                        <span>Premier League - 17 April, 2020</span>
+                      </div>
+                    </div>
+
+                    <div className='col-lg-6 col-md-6'>
+                      <div className='highlights-video'>
+                        <Link
+                          href='#play-video'
+                          onClick={(e) => {
+                            e.preventDefault()
+                            this.openModal()
+                          }}
+                          className='video-btn popup-youtube'
+                        >
+                          <span>Play Video</span>
+                          <i className='flaticon-play-button'></i>
+                        </Link>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </OwlCarousel>
+          ) : (
+            ''
+          )}
+        </div>
+
+        {/* If you want to change the video need to update below videoID */}
+        <ModalVideo
+          channel='youtube'
+          isOpen={this.state.isOpen}
+          videoId='L61p2uyiMSo'
+          onClose={() => this.setState({ isOpen: false })}
+        />
+      </>
+    )
+  }
+}
+
+export default MatcheHighlights
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,37 @@
 'use client'
-import React, { Fragment } from 'react'
-import styles from './page.module.css'
-import Preloader from './components/Preloader'
+import React from 'react'
+import Banner from './components/Banner'
+import NextMatch from './components/NextMatch'
+import UpcomingMatches from './components/UpcomingMatches'
+import MatcheHighlights from './components/MatcheHighlights'
+import Products from './components/Products'
+import Partners from './components/Partners'
+import Gallery from './components/Gallery'
+import Subscribe from './components/Subscribe'
+import BlogPost from './components/BlogPost'
 
-export default function Home() {
+const Football = () => {
   return (
-    <Fragment>
-      <Preloader />
-    </Fragment>
+    <>
+      <Banner />
+
+      <NextMatch />
+
+      <UpcomingMatches />
+
+      <MatcheHighlights />
+
+      <Products />
+
+      <Partners />
+
+      <Gallery />
+
+      <Subscribe />
+
+      <BlogPost />
+    </>
   )
 }
+
+export default Football
